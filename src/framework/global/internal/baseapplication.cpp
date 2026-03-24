dd78ec1a0fb0c60688ada184c5dca63862c94140
tjpadula <tjpadula@earthlink.net>
2026-03-24 14:23:40 -0700

    Defang the restart() method since we can't do that on iOS.

--- src/framework/global/internal/baseapplication.cpp
+++ src/framework/global/internal/baseapplication.cpp
@@ -154,8 +154,12 @@ Qt::KeyboardModifiers BaseApplication::keyboardModifiers() const
 
 void BaseApplication::restart()
 {
+#if defined(Q_OS_IOS)
+    NOT_SUPPORTED;
+#else
     m_finishMode = FinishMode::Restart;
     QCoreApplication::exit();
+#endif
 }
 
 void BaseApplication::finish()
