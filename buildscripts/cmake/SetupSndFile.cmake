b757a538f4df4247219fa6ba2f9dc5565379a6f9
tjpadula <tjpadula@earthlink.net>
2026-03-24 13:59:02 -0700

    Gate Qt::PrintSupport and LinguistTools for iOS.

--- buildscripts/cmake/SetupSndFile.cmake
+++ buildscripts/cmake/SetupSndFile.cmake
@@ -66,7 +66,96 @@ elseif (OS_IS_WASM)
         ${LIBVORBIS_PATH}/lib
     )
 
-else()
+elseif (IOS)
+
+    # IOS_CONFIG_BUG
+    # For the moment, we are going to presume a pre-built libsndfile.a. It will be found at
+    # ${PROJECT_SOURCE_DIR}/ios_libs/[iphoneos|iphonesimulator]/[arm64|x86_64]/ as appropriate.
+    # The header is at ${PROJECT_SOURCE_DIR}/ios_libs/include.
+    #
+    # For 4.6.0a and later, Qt 6.9 is required, but it doesn't build for arm simulator.
+    # However, the Rosetta simulator still works for iOS 16, so we can build to target
+    # that on x86_64. Yes, this is silly.
+    
+    message(STATUS "SetupSndFile.cmake: CMAKE_OSX_SYSROOT: ${CMAKE_OSX_SYSROOT}")
+    message(STATUS "SetupSndFile.cmake: ARCH_IS_X86_64: ${ARCH_IS_X86_64}")
+    message(STATUS "SetupSndFile.cmake: ARCH_IS_AARCH64: ${ARCH_IS_AARCH64}")
+    message(STATUS "SetupSndFile.cmake: QT_HOST_PATH: ${QT_HOST_PATH}")
+    message(STATUS "SetupSndFile.cmake: PLATFORM: ${PLATFORM}")
+    message(STATUS "SetupSndFile.cmake: CMAKE_OSX_ARCHITECTURES: ${CMAKE_OSX_ARCHITECTURES}")
+    message(STATUS "SetupSndFile.cmake: CMAKE_XCODE_SCHEME_ENVIRONMENT: ${CMAKE_XCODE_SCHEME_ENVIRONMENT}")
+    
+    # The abovw gives this on arm64 building for Rosetta simulator:
+# -- SetupSndFile.cmake: CMAKE_OSX_SYSROOT: iphoneos
+# -- SetupSndFile.cmake: ARCH_IS_X86_64:
+# -- SetupSndFile.cmake: ARCH_IS_AARCH64: 1
+# -- SetupSndFile.cmake: QT_HOST_PATH: ~/Code/qt6-693/qt6-build-mac-x86_64/qtbase
+# -- SetupSndFile.cmake: PLATFORM: x86_64
+# -- SetupSndFile.cmake: CMAKE_OSX_ARCHITECTURES: x86_64
+# -- SetupSndFile.cmake: CMAKE_XCODE_SCHEME_ENVIRONMENT: ARCHS=x86_64
+
+    # Use PLATFORM and ARCH_IS_AARCH64 to determine if we're on arm64 building for Rosetta simulator
+
+    if(CMAKE_OSX_SYSROOT MATCHES "imulator")
+        set(LIBSNDFILE_TARGET_OS "iphonesimulator")
+    elseif(CMAKE_OSX_SYSROOT MATCHES "phone")
+        set(LIBSNDFILE_TARGET_OS "iphoneos")
+	else()
+        message(FATAL_ERROR "Unable to determine iOS target for libsndfile.")
+    endif()
+
+    if (ARCH_IS_X86_64)
+        if (LIBSNDFILE_TARGET_OS MATCHES "iphoneos")
+            set(LIBSNDFILE_ARCHITECTURE "arm64")
+        else()
+            set(LIBSNDFILE_ARCHITECTURE "x86_64")
+        endif()
+    elseif(ARCH_IS_AARCH64)
+        # Here is where things get sticky. Arm64 simulator doesn't build for the required
+        # version of Qt, so we build it for x86_64 and run the Rosetta simulator.
+        # Something somewhere is changing LIBSNDFILE_TARGET_OS to 'iphoneos' before the build
+        # gets here. We're done with searching for a needle in s haystack and we're using
+        # a workaround.
+        #if(LIBSNDFILE_TARGET_OS MATCHES "iphonesimulator")
+        if(PLATFORM MATCHES "x86_64")       # We must be building for Rosetta simulator.
+            set(LIBSNDFILE_ARCHITECTURE "x86_64")
+            set(LIBSNDFILE_TARGET_OS "iphonesimulator")
+        else()
+            set(LIBSNDFILE_ARCHITECTURE "arm64")
+        endif()
+    else()
+        message(FATAL_ERROR "Unable to determine iOS processor architecture for libsndfile.")
+    endif()
+
+    message(STATUS "SetupSndFile.cmake: LIBSNDFILE_TARGET_OS: ${LIBSNDFILE_TARGET_OS}")
+    message(STATUS "SetupSndFile.cmake: LIBSNDFILE_ARCHITECTURE: ${LIBSNDFILE_ARCHITECTURE}")
+
+    # Include dir
+    set (LIBSNDFILE_INCLUDE_DIR_SEARCH_PATH ${PROJECT_SOURCE_DIR}/ios_libs/include/)
+    message(STATUS "LIBSNDFILE_INCLUDE_DIR_SEARCH_PATH: ${LIBSNDFILE_INCLUDE_DIR_SEARCH_PATH}")
+ 
+    set(LIBSNDFILE_INCLUDE_DIR ${LIBSNDFILE_INCLUDE_DIR_SEARCH_PATH})
+
+    # Library
+    set (LIBSNDFILE_LIBRARY_SEARCH_PATH ${PROJECT_SOURCE_DIR}/ios_libs/${LIBSNDFILE_TARGET_OS}/${LIBSNDFILE_ARCHITECTURE}/)
+    message(STATUS "LIBSNDFILE_LIBRARY_SEARCH_PATH: ${LIBSNDFILE_LIBRARY_SEARCH_PATH}")
+    
+    set(LIBSNDFILE_LIBRARY "${LIBSNDFILE_LIBRARY_SEARCH_PATH}libsndfile.a")
+
+    if (LIBSNDFILE_LIBRARY AND LIBSNDFILE_INCLUDE_DIR)
+        set(SNDFILE_LIB ${LIBSNDFILE_LIBRARY})
+        set(SNDFILE_INCDIR ${LIBSNDFILE_INCLUDE_DIR})
+        set(SNDFILE_FOUND 1)
+        
+        target_link_libraries(muse_audio PRIVATE ${LIBSNDFILE_LIBRARY})
+
+        message(STATUS "Found libsndfile: ${LIBSNDFILE_LIBRARY}")
+        message(STATUS "Found libsndfile include dir: ${LIBSNDFILE_INCLUDE_DIR_SEARCH_PATH}")
+    endif()
+    
+#        message(FATAL_ERROR "Halt and catch fire.")
+
+ else()
     find_package(SndFile)
 
     if (SNDFILE_FOUND)
@@ -101,7 +190,7 @@ else()
 endif()
 
 if (SNDFILE_INCDIR)
-    message(STATUS "Found sndfile: ${SNDFILE_LIB} ${SNDFILE_INCDIR}")
+    message(STATUS "Found sndfile: ${SNDFILE_LIB} in ${SNDFILE_INCDIR}")
 else ()
     message(FATAL_ERROR "Could not find: sndfile")
 endif ()
